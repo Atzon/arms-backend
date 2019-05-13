343fbac417362adc6b45433c14c92897bddd1abd
kstopyra <kamil.stopyra95@gmail.com>
2019-05-13 19:59:35 +0200

    Changed port number

--- index.js
+++ index.js
@@ -11,5 +11,6 @@ app.use(express.json());
 app.use('/api/points', points);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
