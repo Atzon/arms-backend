00b5e24cae3d6dd0a00c65a6950889c09db9e79b
kstopyra <kamil.stopyra95@gmail.com>
2019-05-18 00:38:43 +0200

    Added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const points = require('./routes/points');
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://arms:arms@arms-mongo-cluster-on2i4.mongodb.net/test?retryWrites=true', { useNewUrlParser: true, useCreateIndex: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use('/api/points', points);
 
