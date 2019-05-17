44558d5034aa27b9deace468b4f5d9803ebaed18
kstopyra <kamil.stopyra95@gmail.com>
2019-05-18 00:38:43 +0200

    Added cors

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const points = require('./routes/points');
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 const mango_url = process.env.MANGO_URL; 
 
 mongoose.connect(mango_url, { useNewUrlParser: true, useCreateIndex: true })
-
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
 
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use('/api/points', points);
 
